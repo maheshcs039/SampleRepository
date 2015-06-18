d87c1c98abaed6c9ca125dc5d3628253b4f7e73a
Mahesha Shivaiah <shivaiah.mahesha@rakuten.com>
2015-06-18 14:48:09 +0900

    Changes tests 2

--- GuildBrowserLogicTests/GuildBrowserLogicTests.m
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.m
@@ -8,16 +8,56 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
-
+#import "Character.h"
+#import "Item.h"
 @interface GuildBrowserLogicTests : XCTestCase
+{
+    NSDictionary *_characterDetailJson;
+    Character *_testGuy;
 
+}
 @end
 
 @implementation GuildBrowserLogicTests
 
 - (void)setUp {
     [super setUp];
+    NSURL *dataServiceURL = [[NSBundle bundleForClass:self.class]
+                             URLForResource:@"character" withExtension:@"json"];
+    
+    // 4
+    NSData *sampleData = [NSData dataWithContentsOfURL:dataServiceURL];
+    NSError *error;
+    
+    // 5
+    id json = [NSJSONSerialization JSONObjectWithData:sampleData
+                                              options:kNilOptions
+                                                error:&error];
+    XCTAssert(json, @"invalid test data");
+    
+    
+    _characterDetailJson = json;
     // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)testCreateCharacterFromDetailJson
+{
+    // 2
+    Character *testGuy1 = [[Character alloc] initWithCharacterDetailData:_characterDetailJson];
+    XCTAssert(testGuy1, @"Could not create character from detail json");
+    
+    // 3
+    Character *testGuy2 = [[Character alloc] initWithCharacterDetailData:nil];
+    XCTAssert(testGuy2, @"Could not create character from nil data");
+}
+-(void)testCreateCharacterFromDetailJsonProps
+{
+    XCTAssert(_testGuy.thumbnail, @"borean-tundra/171/40508075-avatar.jpg", @"thumbnail url is wrong");
+    XCTAssert(_testGuy.name, @"Hagrel", @"name is wrong");
+    XCTAssert(_testGuy.battleGroup, @"Emberstorm", @"battlegroup is wrong");
+    XCTAssert(_testGuy.realm, @"Borean Tundra", @"realm is wrong");
+    XCTAssert(_testGuy.achievementPoints, @"3130", @"achievement points is wrong");
+    
 }
 
 - (void)tearDown {
@@ -36,5 +76,6 @@
         // Put the code you want to measure the time of here.
     }];
 }
+
 
 @end
