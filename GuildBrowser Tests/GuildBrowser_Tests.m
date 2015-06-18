afa5c55424324d877265f18091ca7c29b3e1acdf
Mahesha Shivaiah <shivaiah.mahesha@rakuten.com>
2015-06-18 18:27:38 +0900

    Again created test target

---
+++ GuildBrowser Tests/GuildBrowser_Tests.m
@@ -0,0 +1,40 @@
+//
+//  GuildBrowser_Tests.m
+//  GuildBrowser Tests
+//
+//  Created by Mahesha, Shivaiah on 6/18/15.
+//  Copyright (c) 2015 Charlie Fulton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface GuildBrowser_Tests : XCTestCase
+
+@end
+
+@implementation GuildBrowser_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
